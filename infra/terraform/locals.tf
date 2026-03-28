a0c05df3b0a7ba156a6ddebe6e51f8d1cd3f8af5
Artur Sultanov <sultanov.personal@gmail.com>
2026-03-28 16:04:08 +0100

    chore(): add eks cluster and base worker nodes group

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,8 +1,10 @@
 locals {
-  region       = "us-east-1"
-  zone_1       = "us-east-1a"
-  zone_2       = "us-east-1b"
-  project_name = "devops-demo"
-  eks_name     = "devops-demo-eks"
-  eks_version  = "1.35"
+  region               = "us-east-1"
+  zone_1               = "us-east-1a"
+  zone_2               = "us-east-1b"
+  project_name         = "devops-demo"
+  eks_name             = "devops-demo-eks"
+  eks_version          = "1.35"
+  worker_instance_type = "t3a.medium"
+  worker_capacity_type = "ON_DEMAND"
 }
